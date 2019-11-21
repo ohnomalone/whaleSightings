6bcdf344fcffa5669cfcc2b3010d8a0cda4fc438
Matthew Malone <m.malone@me.com>
2019-11-20 18:25:13 -0700

    Complete node setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const url = require('url');
+const server = http.createServer();
+
+server.listen(3000, () => {
+  console.log('The HTTP server is listening at Port 3000.');
+});
+
+server.on('request', (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.write('Hello World, What is up??\n');
+  response.end();
+});
