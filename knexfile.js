75e62268bf4b98e574108e30b03b2ce427f210b1
Matthew Malone <m.malone@me.com>
2019-11-22 10:11:48 -0700

    Update Knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,20 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/whalewatchers',
-    migrations: {
-     directory: './db/migrations'
-   },
-  seeds : {
-    directory: '/.db/seeds/dev'
-  },
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL + `?ssl=true`,
+    connection: 'postgres://localhost/legos',
     migrations: {
       directory: './db/migrations'
     },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
     useNullAsDefault: true
+  },
+  production: {
+  client: 'pg',
+  connection: process.env.DATABASE_URL + `?ssl=true`,
+  migrations: {
+    directory: './db/migrations'
   },
   useNullAsDefault: true
   }
